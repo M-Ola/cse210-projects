e58d1d3f4c5cffaedceaef69e1c80c5d4ac1311b
ola466@gmail.com <ola466@gmail.com>
2025-03-09 17:58:38 -0500

    finished c# prep 03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,57 @@
 using System;
+// Author: Monsuru Oladipo. CSE 210
+// Exercise 3 Core requirement
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+
+        //Console.Write("What is your magic number? ");
+
+        // Console.Write("What is your guess  number? ");
+
+        Random random = new();
+        int magicNumber = random.Next(1, 101);
+
+
+        int guess = -1;
+       
+       
+       
+       // Console.Write("What is your magic number? ");
+      //  magicNumber = int.Parse(Console.ReadLine());
+
+        while (guess != magicNumber)
+        {
+
+
+            Console.Write("What is your guess number? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (guess > magicNumber)
+            {
+
+                Console.WriteLine("Lower");
+
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+
+            }
+            else
+            {
+                Console.WriteLine("You guesed it!");
+            }
+
+
+
+        }
+
+
+        
     }
 }
