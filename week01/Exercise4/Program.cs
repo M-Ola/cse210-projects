5b4020f5e3f8099d47cab64556334c4efcc6d2c2
ola466@gmail.com <ola466@gmail.com>
2025-03-09 20:08:39 -0500

    Finished C# prep 04

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,71 @@
 using System;
+// Author: Monsuru Oladipo. CSE 210
+// Exercise 4 Core requirement
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+
+        List<int> numbers = [];
+
+
+
+        int number = -1;
+
+        while (number != 0)
+        {
+
+            Console.Write("Enter number: Type 0 when done ");
+            number = int.Parse(Console.ReadLine());
+
+
+
+
+            if (number != 0)
+            {
+
+                numbers.Add(number);
+
+
+            }
+
+        }
+
+
+
+        int sum = 0;
+        int average = 0;
+        int largest = 0;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+
+            average = sum / numbers.Count;
+            if (num > largest)
+            {
+                largest = num;
+
+            }
+
+
+        }
+
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The Average is: {average}");
+        Console.WriteLine($"The Largest numbrer is: {largest}");
+
+
+
+
+
+
+
     }
 }
